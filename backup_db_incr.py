dd0848d6ca7f52d374c4b198576f4444951ae697
rind-tran <53333438+rind-tran@users.noreply.github.com>
2023-06-20 15:10:04 +0700

    Update backup_db_incr.py
    Use BranchPythonOperator to schedule job.

--- backup_db_incr.py
+++ backup_db_incr.py
@@ -12,6 +12,27 @@ from airflow.providers.ssh.operators.ssh import SSHOperator
 
 # Operators;
 from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator, BranchPythonOperator
+
+# Dummy
+from airflow.operators.empty import EmptyOperator
+
+
+# Variables
+tabDays = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"]
+
+
+# returns the week day (monday, tuesday, etc.)
+def get_day(**kwargs):
+    kwargs['ti'].xcom_push(key='day', value=datetime.now().weekday())
+
+# check if today is from monday to saturday, returns the name id of the task to launch (backup_task, no_task, etc.)
+def branch(**kwargs):
+    today = tabDays[kwargs['ti'].xcom_pull(task_ids='weekday', key='day')]
+    if today == 'sunday':
+        return 'no_task'
+    else:
+        return 'backup_db_incr'
 
 sshHook = SSHHook(ssh_conn_id="dbgenesys03_ssh")
 
@@ -39,24 +60,41 @@ with DAG(
         # 'sla_miss_callback': yet_another_function, # or list of functions
         # 'trigger_rule': 'all_success'
     },
-    description="dbgenesys03 backup db incr",
+    description="dbgenesys03 backup archivelog",
     start_date=pendulum.datetime(2023, 6, 14, tz="Asia/Saigon"),
-    schedule_interval='30 23 * * 1-6',
+    schedule_interval='30 12,21 * * *',
     catchup=False,
     tags=["dbgenesys03","genesys"],
 ) as dag:
     command_01="/home/oracle/bin/main_backup_database_incremental_level.sh gsvn1 inc > /dev/null 2>&1 "
+   
 
+    # Tasks
+    # PythonOperator will retrieve and store into "weekday" variable the week day
+    get_weekday = PythonOperator(
+    task_id='weekday',
+    python_callable=get_day,
+    provide_context=True,
+    dag=dag
+    )
 
-    # t1, t2 and t3 are examples of tasks created by instantiating operators
-    t1 = SSHOperator(
+    # BranchPythonOperator will use "weekday" variable, and decide which task to launch next
+    fork = BranchPythonOperator(
+    task_id='branching',
+    python_callable=branch,
+    provide_context=True,
+    dag=dag)
+
+    backup = SSHOperator(
         task_id="backup_db_incr",
         command=command_01,
         ssh_hook=sshHook,
         cmd_timeout=None,
     )
 
-   
+    # task 1, get the week day
+    get_weekday.set_downstream(fork)
 
-
-    # t1 >> t2 >> t3
+    # Monday - Saturday: backup db incr, Sunday: do nothing 
+    fork.set_downstream(backup)
+    fork.set_downstream(EmptyOperator(task_id='no_task', dag=dag))
